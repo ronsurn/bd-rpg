8d6dd9bc26deaa55b78b89bd85806512d5848bb9
ronsurn <imron99@hotmail.com>
2026-01-09 11:34:16 +0700

    Initial empty commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        index2: resolve(__dirname, 'index2.html'),
+      },
+    },
+  },
+})
